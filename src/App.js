780a99674a5c2a30bd103c83020aac52e81518c7
Rajshree <120099588+Rajshree36@users.noreply.github.com>
2024-04-25 10:47:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=1a538e5d3d6fb756970f9e9477ab2df1`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
